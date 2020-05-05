84b59a7f0bbad419452d7c465010d52aeb6f633a
AndroidLoaders <codes1589@gmail.com>
2020-05-05 20:40:40 +0530

    Added DI in my own Api Calling Retrofit Structure, Added Extension functions for better performance

---
+++ app/src/main/java/com/example/mvvm_dagger/networkadapter/apiconstants/ApiProvider.kt
@@ -0,0 +1,9 @@
+package com.example.mvvm_dagger.networkadapter.apiconstants
+
+import com.example.mvvm_dagger.BuildConfig
+
+object ApiProvider {
+
+    private const val GET_LOGIN_USER = "588d14f4100000a9072d2943"
+    const val ApiGetLoginUser = BuildConfig.BASE_URL + GET_LOGIN_USER
+}
