8fceb37c123d9cbd6b6f58b4759639ce895e017e
AndroidLoaders <codes1589@gmail.com>
2020-05-07 13:12:12 +0530

    Managed Shared Preference Data

--- app/src/main/java/com/example/mvvm_dagger/base/extensions/rx/rxview/DisposibleExtensions.kt
+++ app/src/main/java/com/example/mvvm_dagger/base/extensions/rx/rxview/DisposibleExtensions.kt
@@ -1,6 +1,2 @@
 package com.example.mvvm_dagger.base.extensions.rx.rxview
 
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-fun Disposable.autoDispose(compositeDisposable: CompositeDisposable) = compositeDisposable.add(this)
